edca0d960e18f0c0f44bd9b98a012aca55ba75d9
Saif Kazi <saif.zane2@gmail.com>
2021-01-29 10:39:42 +0400

    modified cookie and shots

--- Screenshot.py
+++ Screenshot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+
 driver = webdriver.Chrome(executable_path="D:/Installers/chromedriver_v88/chromedriver.exe")
 
 driver.get("https://www.google.com/")
@@ -7,6 +8,6 @@ driver.get("https://www.google.com/")
 driver.save_screenshot("D:/google.png")
 time.sleep(1)
 
-driver.get_screenshot_as_file("D:/google.jpg") # Will issue warnings
+driver.get_screenshot_as_file("D:/google.jpg")  # Will issue warnings
 
 driver.quit()
